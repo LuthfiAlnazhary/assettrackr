fa2e3900a98a3d5d140438369f7c8457cf3f8a86
alvitoathallah <alvitoathallah@gmail.com>
2024-10-06 14:55:24 +0700

    refactoring web security config

--- Assettrackr/src/main/java/propensi/project/Assettrackr/security/WebSecurityConfig.java
+++ Assettrackr/src/main/java/propensi/project/Assettrackr/security/WebSecurityConfig.java
@@ -3,45 +3,50 @@ package propensi.project.Assettrackr.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import propensi.project.Assettrackr.security.jwt.JwtTokenFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/css/**").permitAll()
-                .antMatchers("/js/**").permitAll()
-                .antMatchers("/api/**").permitAll()
-                .antMatchers("/login-sso", "/validate-ticket").permitAll()
-                .antMatchers(
-                        "/manager/viewall",
-                        "/manager/delete",
-                        "/sponsor/add",
-                        "/sponsor/viewall",
-                        "/sponsor/delete/*",
-                        "/sales/add",
-                        "/sales/viewall",
-                        "/sales/delete/*"
-                ).hasAuthority("Admin")
-                .antMatchers("/sales/kontrak/*").hasAuthority("Sales")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login").permitAll();
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(this::configureAuthorization)
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    private void configureAuthorization(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry requests) {
+        requests
+                .requestMatchers(new AntPathRequestMatcher("/api/v1/user/login")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/v1/user/logout")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/v1/user/create")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/v1/admin/**")).hasAuthority("Admin")
+                .requestMatchers(new AntPathRequestMatcher("/api/v1/user/**")).hasAuthority("Admin")
+                .anyRequest().authenticated();
     }
 
     @Bean
@@ -49,13 +54,12 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder())
+                .and()
+                .build();
     }
 }
-
-
